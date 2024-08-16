6cd62484f8e14ffcc40223bd0b431bcd7f8bdb0b
garyzhang75 <166787037+garyzhang75@users.noreply.github.com>
2024-08-16 16:11:55 +1000

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import NewPostPage from './newpage';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<div>
+  <NewPostPage />
+</div>
+);
